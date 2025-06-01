f2cab497837180c6ff2f006b0a7a935f126fa6ca
seb <ciuisebi@gmail.com>
2025-06-01 16:26:23 +0300

    Add main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,34 @@
-package iss
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import database.DatabaseHelper
+import gui.FitnessLevelForm
+import gui.navigation.RunningTrainingApp
+import kotlinx.serialization.json.Json
+import org.hibernate.SessionFactory
+import org.hibernate.cfg.Configuration
+import repository.impl.TrainingDayRepositoryImpl
+import repository.impl.TrainingPlanRepositoryImpl
+import repository.impl.UserRepositoryImpl
+import service.openapi.GeminiConfig
+import service.openapi.GeminiServiceImpl
+import service.server.ServiceImpl
+import java.io.FileNotFoundException
+import java.util.*
 
-fun main() {
+fun main() = application {
 
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Running Training App"
+    ) {
+        MaterialTheme {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                RunningTrainingApp()
+            }
+        }
+    }
 }
